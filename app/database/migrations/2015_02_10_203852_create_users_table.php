92dd514006855ace03f4ba84359cf4b5faa5baf6
Alleo Indong <freedom_war550@live.com.ph>
2015-02-11 04:55:23 +0800

    removed email and use employee_no as the username

--- app/database/migrations/2015_02_10_203852_create_users_table.php
+++ app/database/migrations/2015_02_10_203852_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration {
 			$table->string('employee_no')->unique();
 			$table->integer('department_id');
 			$table->integer('role_id');
-			$table->string('email')->unique();
 			$table->string('password');
 			$table->softDeletes();
 			$table->timestamps();
