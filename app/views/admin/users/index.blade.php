bfe5288a32d57bfe7b8f4c1c139589763cd0cbea
Alleo Indong <freedom_war550@live.com.ph>
2015-02-11 21:20:54 +0800

    Linked users and Departments

--- app/views/admin/users/index.blade.php
+++ app/views/admin/users/index.blade.php
@@ -19,6 +19,7 @@
             <td>{{{ $user->employee_no }}}</td>
             <td>{{{ ucfirst($user->department->name) or 'N/A'}}}</td>
             <td>{{{ ucfirst($user->role->name) }}}</td>
+            <td>{{{ date('M d Y', strtotime($user->created_at)) }}}</td>
             <td>
                 <a href="/admin/users/{{ $user->id }}/edit" class="btn btn-warning">Update</a>
                 {{ Form::open(array('url' => 'admin/articles/' . $user->id, 'class' => 'deleteItem form-inline')) }}
