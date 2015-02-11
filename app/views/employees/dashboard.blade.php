f0faa47828338c01e5c6c4b4fb1fcc677cf37947
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-02-12 00:44:13 +0800

    employee dashboard

--- app/views/employees/dashboard.blade.php
+++ app/views/employees/dashboard.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.default')
 
 @section('content')
+    <h2>Job Vacancies </h2>
+    <table class="table table-striped table-bordered" cellspacing="0" width="100%" id="example">
+        <thead>
+        <tr>
+            <th>Title</th>
+            <th>Created</th>
+            <th>Updated</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($jobvacancies as $jobvacancy)
+        <tr>
+            <td>{{ $jobvacancy->title }}</td>
+            <td>{{ date('M d Y', strtotime($jobvacancy->created_at)) }}</td>
+            <td>{{ date('M d Y', strtotime($jobvacancy->created_at)) }}</td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+@stop
 
+@section('page-script')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var table = $('#example').DataTable();
+        });
+    </script>
 @stop
