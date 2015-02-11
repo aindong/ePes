647673795aa9079114120deabd1c0549ba31031f
Alleo Indong <freedom_war550@live.com.ph>
2015-02-11 23:02:16 +0800

    Cleaning

--- app/views/admin/users/edit.blade.php
+++ app/views/admin/users/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     {{ Form::model($user, ['route' => ['admin.users.update', $user->id], 'method' => 'put']) }}
-    <legend>Creating a new User</legend>
-    @include('admin.users.form')
+        <legend>Creating a new User</legend>
+        @include('admin.users.form')
     {{ Form::close() }}
 @stop
