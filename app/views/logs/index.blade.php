9d882b1049b851c41879596f6bcc4c698240be0a
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-02-12 00:11:18 +0800

    logs

--- app/views/logs/index.blade.php
+++ app/views/logs/index.blade.php
@@ -1,1 +1,65 @@
-C:\Users\papabear\Desktop\server\ePes\app/views/logs/index.blade.php
+@extends('layouts.default')
+
+@section('content')
+    <h2>Audit Trail Management </h2>
+    <table class="table table-striped table-bordered" cellspacing="0" width="100%" id="example">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Details</th>
+            <th>Created</th>
+            <th>Updated</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($logs as $log)
+        <tr>
+            <td>{{ ucfirst($log->user_id) }}</td>
+            <td>{{ ucfirst($log->action) }}</td>
+            <td>{{ date('M d Y', strtotime($log->created_at)) }}</td>
+            <td>{{ date('M d Y', strtotime($log->updated_at)) }}</td>
+            <td>
+                <a href="#" class="deleteItem btn btn-danger" data-item="{{ $log->id }}">Delete</a>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+@stop
+
+@section('page-script')
+    <script type="text/javascript">
+            $(document).ready(function() {
+                var table = $('#example').DataTable();
+
+                $('.deleteItem').on('click', function(e) {
+                    e.preventDefault();
+
+                    if(!confirm('Are you sure to delete this item?')) {
+                        return false;
+                    }
+                    var id = $(this).attr('data-item');
+                    var $that = $(this);
+
+                    var url = location.href;
+
+                    if(url.substr(-1) == '/') {
+                        url = url.substr(0, url.length - 1);
+                    }
+
+                    $.ajax({
+                        url:  url + '/' + id,
+                        type:"post",
+                        data: { _method:"DELETE" },
+                        success: function(data) {
+                            alert('Item successfully deleted');
+                            //location.reload();
+                            var rowSelected = $that.parent().parent();
+                            table.row(rowSelected).remove().draw();
+                        }
+                    });
+                });
+            });
+    </script>
+@stop
