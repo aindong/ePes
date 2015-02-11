6481f2adf6c651bc0b43d98edfc8b8c15525788b
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-02-11 21:13:08 +0800

    index celebration and department

--- app/views/admin/celebrations/index.blade.php
+++ app/views/admin/celebrations/index.blade.php
@@ -1,1 +1,46 @@
-C:\Users\papabear\Desktop\server\ePes\app/views/celebrations/index.blade.php
+@extends('layouts.default')
+
+@section('content')
+    <h2>Events Management <a href="/admin/celebrations/create" class="btn btn-primary">Add New</a></h2>
+    <table class="table table-striped table-bordered" cellspacing="0" width="100%" id="example">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Created</th>
+            <th>Updated</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($celebrations as $celebration)
+        <tr>
+            <td>My name</td>
+            <td>testing</td>
+            <td>testing</td>
+            <td>
+                <a href="/admin/celebrations/1/edit" class="btn btn-warning">Update</a>
+                <!-- <a href="#" class="btn btn-danger">Delete</a> -->
+                {{ Form::open(array('url' => 'admin/celebrations/' . 1, 'class' => 'deleteItem')) }}
+                {{ Form::hidden('_method', 'DELETE') }}
+                {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
+                {{ Form::close() }}
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+@stop
+
+@section('page-script')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#example').dataTable();
+
+            $('.deleteItem').on('submit', function(e) {
+                if(!confirm('Are you sure to delete this item?')) {
+                    return false;
+                }
+            });
+        });
+    </script>
+@stop
--- app/views/admin/departments/index.blade.php
+++ app/views/admin/departments/index.blade.php
@@ -1,34 +1,32 @@
 @extends('layouts.default')
 
 @section('content')
-    <h2>Article Management <a href="/admin/articles/create" class="btn btn-primary">Add New</a></h2>
+    <h2>Department Management <a href="/admin/departments/create" class="btn btn-primary">Add New</a></h2>
     <table class="table table-striped table-bordered" cellspacing="0" width="100%" id="example">
         <thead>
         <tr>
             <th>Name</th>
-            <th>Title</th>
             <th>Created</th>
             <th>Updated</th>
             <th>Actions</th>
         </tr>
         </thead>
         <tbody>
-        {{--@foreach($articles as $article)--}}
+        @foreach($departments as $department)
         <tr>
-            <td>My name</td>
-            <td>testing</td>
-            <td>testing</td>
-            <td>testing</td>
+            <td>{{ department->name }}</td>
+            <td>{{ department->created_at }}</td>
+            <td>{{ department->updated_at }}</td>
             <td>
-                <a href="/admin/articles/1/edit" class="btn btn-warning">Update</a>
+                <a href="/admin/departments/1/edit" class="btn btn-warning">Update</a>
                 <!-- <a href="#" class="btn btn-danger">Delete</a> -->
-                {{ Form::open(array('url' => 'admin/articles/' . 1, 'class' => 'deleteItem')) }}
+                {{ Form::open(array('url' => 'admin/departments/' . 1, 'class' => 'deleteItem')) }}
                 {{ Form::hidden('_method', 'DELETE') }}
                 {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
                 {{ Form::close() }}
             </td>
         </tr>
-        {{--@endforeach--}}
+        @endforeach
         </tbody>
     </table>
 @stop
