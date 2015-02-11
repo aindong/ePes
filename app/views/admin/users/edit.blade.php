acb77bf5c36f95b2b8d8aba2bfc209470b3f0927
Alleo Indong <freedom_war550@live.com.ph>
2015-02-11 23:00:06 +0800

    Updating a user

--- app/views/admin/users/edit.blade.php
+++ app/views/admin/users/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-    {{ Form::model($user, ['route' => 'admin.users.store', 'method' => 'post']) }}
+    {{ Form::model($user, ['route' => ['admin.users.update', $user->id], 'method' => 'put']) }}
     <legend>Creating a new User</legend>
     @include('admin.users.form')
     {{ Form::close() }}
