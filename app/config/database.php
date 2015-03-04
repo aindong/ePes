c8df622bba30c4d34ecf8f6e99d78ebe0ffaafda
Alleo Indong <freedom_war550@live.com.ph>
2015-03-05 02:02:29 +0800

    Updated database

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'epes',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'molecules',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
