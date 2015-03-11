a050ecf8933af830c955745038cf9d4416106385
Alleo Indong <freedom_war550@live.com.ph>
2015-03-12 03:02:21 +0800

    test

--- app/views/layouts/login.blade.php
+++ app/views/layouts/login.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
 
-  <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>Tarlac Online Employee Performance Evaluation System</title>
 
@@ -155,6 +157,13 @@ body{
 	margin-top: -35px;
 }
 </style>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 	<link rel="stylesheet" href="/packages/bootstrap/css/bootstrap.min.css"/>
     <script src="/js/prefixfree.min.js"></script>
