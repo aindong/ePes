2aa360fd9ff4a1413623fb33c960e1a7e796ab32
Alleo Indong <freedom_war550@live.com.ph>
2015-03-12 21:58:03 +0800

    test

--- app/views/admin/evaluations/create.blade.php
+++ app/views/admin/evaluations/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-    <legend>Create a new department</legend>
+    <legend>Create a new evaluation schedule</legend>
     {{ Form::open(['route' => 'admin.evaluations.store', 'method' => 'post']) }}
     @include('admin.evaluations.form')
     {{ Form::close() }}
